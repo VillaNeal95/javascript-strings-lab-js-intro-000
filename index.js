ef13f36011a74d94f44e1c0074f35747be51bbbf
Neal Makh <nealmakh@hotmail.co.uk>
2019-11-21 03:48:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Hello, Hello, Hello, Hello, Neil deGrasse Tyson!!"
 
-var greetSpecialGuest = "Hello, Hello, Hello, Hello, Hello," + specialGuest + "!!!";
+var greetSpecialGuest = `"Hello, Hello, Hello, Hello, Hello," + specialGuest + "!!!"`;
 
 var topic = "space";
 
